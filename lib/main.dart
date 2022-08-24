063b6f19577c348f2d9842f188e2f1063d6d2407
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-24 12:04:53 +0500

    Code Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,42 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tasker/app_route/app_route_name.dart';
 import 'package:tasker/app_route/app_router.dart';
+import 'package:tasker/app_utils/scroll_config.dart';
 
 import 'app_controller_binding.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MyApp(),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: Size(375, 812),
+        designSize: const Size(375, 812),
         minTextAdapt: true,
         splitScreenMode: false,
         builder: (context, _) {
           return GetMaterialApp(
             title: 'Test',
+            debugShowCheckedModeBanner: false,
             initialBinding: AppControllerBinding(),
-            debugShowCheckedModeBanner: true,
             initialRoute: '/${AppRouteName.rostrInner}',
             getPages: AppRouter.onGeneratePage(),
+
+            // * To remove glow effect on the whole application
+            builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+                child: ScrollConfiguration(
+                  behavior: NoGlowBehavior(),
+                  child: child!,
+                ),
+              );
+            },
           );
         });
   }
