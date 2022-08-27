8eb1753b16e29d1f5856730e566e6d0495bf08c7
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-27 19:50:02 +0500

    Code Update

--- lib/data/models/rating_model.dart
+++ lib/data/models/rating_model.dart
@@ -1,5 +1,15 @@
-class Ratings {
+class Rating {
   String title;
   double score;
-  Ratings({required this.title, this.score = 0});
+
+  Rating({required this.title, this.score = 0});
+
+  Rating.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        score = json['score'];
+
+  Map<String, dynamic> toJson() => {
+    'title' : title,
+    'score' : score,
+  };
 }
