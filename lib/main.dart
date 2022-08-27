fa1bdc45d8db8b976fc03a553e141790bdd03945
Tukhtamurodov-Sardorbek <sardortukhtamurodov02@gmail.com>
2022-08-27 21:06:02 +0500

    Code Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:tasker/app_route/app_route_name.dart';
 import 'package:tasker/app_route/app_router.dart';
 import 'package:tasker/app_utils/scroll_config.dart';
+import 'package:tasker/data/local_database.dart';
 import 'app_controller_binding.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox(LocalDatabase.DB_NAME);
   runApp(const MyApp());
 }
 
